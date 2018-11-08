370de9b1528ee65f073f329541f0b407218c1e4a
Glenn Francis <XglennfrancisX@gmail.com>
2018-11-08 11:23:36 +1000

    added required image

--- db/schema.rb
+++ db/schema.rb
@@ -111,6 +111,14 @@ ActiveRecord::Schema.define(version: 2018_11_07_035225) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  create_table "users_roles", id: false, force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "role_id"
+    t.index ["role_id"], name: "index_users_roles_on_role_id"
+    t.index ["user_id", "role_id"], name: "index_users_roles_on_user_id_and_role_id"
+    t.index ["user_id"], name: "index_users_roles_on_user_id"
+  end
+
   add_foreign_key "comments", "listings"
   add_foreign_key "comments", "users"
   add_foreign_key "favorites", "users"
