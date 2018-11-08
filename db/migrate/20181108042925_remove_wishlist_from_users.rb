c4d348a3382058c79c4869a1daf16c83e7467d96
Glenn Francis <XglennfrancisX@gmail.com>
2018-11-08 15:44:41 +1000

    fixed wishlist migration

--- db/migrate/20181108042925_remove_wishlist_from_users.rb
+++
@@ -1,5 +0,0 @@
-class RemoveWishlistFromUsers < ActiveRecord::Migration[5.2]
-  def change
-    remove_column :users, :wishlist_id, :bigint
-  end
-end
--- db/migrate/20181108043048_drop_wishlists_table.rb
+++
@@ -1,8 +0,0 @@
-class DropWishlistsTable < ActiveRecord::Migration[5.2]
-  def up
-    drop_table :wishlists
-  end
-  def down
-    raise ActiveRecord::IrreversibleMigration
-  end
-end
